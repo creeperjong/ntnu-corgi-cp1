8fab1ec087036490d62fffaa07679f05151a265e
creeperjong <leogood50203@gmail.com>
2020-12-26 23:52:52 +0800

    init

---
+++ playground/alloc2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(){
+    int32_t * a[9] = {0};
+
+    for( size_t i = 0 ; i < 9 ; i++ )
+    {
+
+        a[i] = malloc( 9 * sizeof( int32_t ) );
+
+        for( size_t j = 0 ; j < 9 ; j++ )
+        {
+            a[i][j] = ( i + 1 ) * ( j + 1 );
+        }
+    }
+
+    /*for( size_t i = 0 ; i < 9 ; i++ )
+    {
+        for( size_t j = 0 ; j < 9 ; j++ )
+        {
+        printf( "%02d ", a[i][j] );
+        }
+
+        printf( "\n" );
+    }*/
+
+    for( size_t i = 0 ; i < 9 ; i++ )
+    {
+        printf("The address of row %ld is %p\n",i,&a[i]);
+        printf("The value of row %ld is %p\n",i,a[i]);
+        for( size_t j = 0 ; j < 9 ; j++ )
+        {
+            printf( "   The address of column %ld is %p\n",j,&a[i][j]);
+        }
+    }
+
+
+    return 0;
+}
